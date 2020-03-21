367e16575acb10f87236d4305201d1b4ddd1f54f
wkuch <wanjakuch@gmail.com>
2020-03-22 00:29:20 +0100

    Gallery Preview started

--- src/components/container/Router/Router.jsx
+++ src/components/container/Router/Router.jsx
@@ -9,12 +9,14 @@ import Home from '../../pages/Home/Home'
 import Contact from '../../pages/Contact/Contact'
 import Layout from '../Layout/Layout'
 import { NotFound } from '../../pages/NotFound/NotFound'
+import Gallery from '../../pages/Gallery/Gallery'
 
 export const RouterConfig = () => (
   <Router>
     <Layout>
       <Switch>
         <Route exact path='/kontakt' component={Contact} />
+        <Route exact path='/gallerie' component={Gallery} />
         <Route exact path='/' component={Home} />
         <Route component={NotFound} />
       </Switch>
