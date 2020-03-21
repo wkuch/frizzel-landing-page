a28bb564eb00f2e4f2b645f8ac4581c73428a127
wkuch <wanjakuch@gmail.com>
2020-03-21 16:33:04 +0100

    lint

--- src/helper/frizzelApiConnection.js
+++ src/helper/frizzelApiConnection.js
@@ -1,6 +1,7 @@
 
 export const get = (resourceURI, callback) => {
   const baseUrl = window.apiURL
+  const fetch = window.fetch
   fetch(baseUrl + resourceURI)
     .then(response => {
       return response.json()
