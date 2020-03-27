09063d071b5b18e90d8ee4fc9995c4ca0e6e9f13
wkuch <wanjakuch@gmail.com>
2020-03-27 22:28:46 +0100

    added href functionality to NavLink

--- src/components/atoms/NavLink/NavLink.jsx
+++ src/components/atoms/NavLink/NavLink.jsx
@@ -2,14 +2,29 @@ import React from 'react'
 import c from 'classnames'
 import { Link } from 'react-router-dom'
 
-export const NavLink = props => (
-  <div className='nav-item'>
-    <li className={c([
-      'mx-4',
-      props.active && 'active'
-    ])}
-    >
-      <Link className='nav-link h1' to={props.to}>{props.text}</Link>
-    </li>
-  </div>
-)
+export const NavLink = props => {
+  console.log(props.href)
+  return (
+
+    <div className='nav-item'>
+      <li className={c([
+        'mx-4',
+        props.active && 'active'
+      ])}
+      >
+        {renderLink(props)}
+      </li>
+    </div>
+  )
+}
+
+const renderLink = (props) => {
+  if (props.href) {
+    return (
+      <a className='nav-link h1' target='_blank' href={props.href} >{props.text}</a>
+    )
+  }
+  return (
+    <Link className='nav-link h1' to={props.to} >{props.text}</Link>
+  )
+}
