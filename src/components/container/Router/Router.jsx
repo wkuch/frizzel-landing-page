dc37ef8ea5202b159306265a15b0d9d162d1d510
wkuch <wanjakuch@gmail.com>
2020-05-09 14:56:29 +0200

    added impressum

--- src/components/container/Router/Router.jsx
+++ src/components/container/Router/Router.jsx
@@ -9,13 +9,17 @@ import Home from '../../pages/Home/Home'
 import Layout from '../Layout/Layout'
 import { NotFound } from '../../pages/NotFound/NotFound'
 import { DataProtection } from '../../pages/DataProtection/DataProtection'
+import { Impressum } from '../../pages/Impressum/Impressum'
+
 
 export const RouterConfig = () => (
   <Router>
+    
     <Layout>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/data-protection' component={DataProtection} />
+        <Route exact path='/impressum' component={Impressum} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
