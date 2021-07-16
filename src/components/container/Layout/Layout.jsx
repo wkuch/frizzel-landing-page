264e1748cc2b034e744742c6332e5f5e3d5f8465
Wanja Kuch <53600785+wkuch@users.noreply.github.com>
2021-07-16 22:21:57 +0200

    Update Layout.jsx
    fix typo

--- src/components/container/Layout/Layout.jsx
+++ src/components/container/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Header } from '../../molecules/Header/Header'
 import { Footer } from '../../molecules/Footer/Footer'
-import Tabete from '../../../assets/backgrounds/tapete.jpg'
+import Tapete from '../../../assets/backgrounds/tapete.jpg'
 import Logo from '../../../assets/logo/frizzel-logo.png'
 export const Layout = (props) => {
   return (
     <div>
-      <div className='bg' style={{ backgroundImage: `url(${Tabete})` }} />
+      <div className='bg' style={{ backgroundImage: `url(${Tapete})` }} />
       <div>
         <div className='container flex-column d-flex col-10 col-md-7 col-lg-3 justify-content-center mb-5'>
           <img className='mx-auto my-5 img-fluid' src={Logo} alt='Frizzel-Logo' />
